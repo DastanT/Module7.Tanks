1f9f8df4f9a96a6c966ecaacbfc19806b51455dd
Dastan <dastan.smarti@gmail.com>
2021-04-10 10:28:51 +0600

    Add project files.

---
+++ Module7.Tanks/Program.cs
@@ -0,0 +1,66 @@
+﻿using MyClassLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module7.Tanks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            List<Tank> t34 = new List<Tank>();
+            List<Tank> panteras = new List<Tank>();
+            Tank t_34 = new Tank();
+            Tank pantera = new Tank();
+            Tank result = new Tank();
+
+            int countOfTanks = 5;
+
+            for (int i = 0; i < countOfTanks; i++)
+            {
+                t_34 = new Tank("T-34", rnd.Next(1, 100), rnd.Next(1, 100), rnd.Next(1, 100));
+                pantera = new Tank("Pantera", rnd.Next(1, 100), rnd.Next(1, 100), rnd.Next(1, 100));
+                //tanks.Add(t_34);
+                //tanks.Add(pantera);
+                t34.Add(t_34);
+                panteras.Add(pantera);
+
+            }
+
+            War(t34, panteras, countOfTanks);
+
+
+
+        }
+        static void War(List<Tank> t34, List<Tank> panteras, int countOfTanks)
+        {
+            for (int i = 0; i < countOfTanks; i++)
+            {
+                Console.WriteLine("Пара №{0}", i + 1);
+                Tank.PrintInfo(t34[i], panteras[i]);
+
+                if (t34[i] ^ panteras[i])
+                {
+                    Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Победитель сражения: T-34\n");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Победитель сражения: Pantera\n");
+                    Console.ResetColor();
+                }
+
+
+            }
+        }
+
+    }
+
+
+}
---
+++ MyClassLib/Tank.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib
+{
+    public class Tank
+    {
+        public string TankModel;
+        private int Ammunition;
+        private int Armor;
+        private int Mobility;
+
+        public Tank() { }
+        public Tank(string TankModel, int Ammunition, int Armor, int Mobility)
+        {
+            this.TankModel = TankModel;
+            this.Ammunition = Ammunition;
+            this.Armor = Armor;
+            this.Mobility = Mobility;
+        }
+
+        public static void PrintInfo(Tank obj1, Tank obj2)
+        {
+            string space = new String(' ', 25 - obj1.ToString().Length);
+
+            string ff = string.Format($"Танк: {obj1.TankModel}\t\t{space}Танк: {obj2.TankModel}\n" +
+                $"Боекомлект: {obj1.Ammunition}\t\t{space}Боекомлект: {obj2.Ammunition}\n" +
+                $"Броня: {obj1.Armor}\t\t{space}Броня: {obj2.Armor}\n" +
+                $"Маневренность: {obj1.Mobility}\t{space}Маневренность:{obj2.Mobility}");
+            Console.WriteLine(ff);
+            //Console.WriteLine($" Танк: " + obj1.TankModel+"\t"+{space}+obj2.TankModel);
+            //Console.WriteLine(" Боекомлект: " + obj1.Ammunition+"t"+obj2.Ammunition);
+            //Console.WriteLine(" Броня: " + obj1.Armor + "\t"+obj2.Armor);
+            //Console.WriteLine(" Маневренность: " + obj1.Mobility + "\t" + obj2.Mobility);
+            
+            //Console.WriteLine();
+        }
+
+        public static bool operator ^(Tank t34, Tank pantera)
+        {
+            int supermacyT34 = 0;
+            int supermacyPantera = 0;
+
+            if (t34.Ammunition > pantera.Ammunition) supermacyT34++;            
+
+            else supermacyPantera++;
+            if (t34.Armor > pantera.Armor) supermacyT34++;
+
+            else supermacyPantera++;
+            if (t34.Mobility > pantera.Mobility) supermacyT34++;
+
+            else supermacyPantera++;
+
+
+            return supermacyT34 >= supermacyPantera;                
+        }
+    }
+
+
+
+        
+}
